217d4e4e20397be6f5c72d0c61c025a0ac0d617f
agrevster <138630000+agrevster@users.noreply.github.com>
2025-09-26 19:10:51 -0500

    Added stocks module as well as a custom stock api using Python to pull

--- scripts/README.md
+++ scripts/README.md
@@ -11,3 +11,13 @@
 3. Run the script and enter this info.
 4. Congrats! You now have a `PPM` logo for your desired team.
 
+## `stock-api.py`
+> Used to serve a simple api that is used for pulling stock information for the stocks module.
+
+*Serves on `127.0.0.1:9820`*
+
+### Usage:
+1. Install decencies: `pip install waitress flask yfinance`
+2. Run the server in the background or with a rc-service like `local`.
+3. Enjoy!
+
---
+++ scripts/stock-api.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify
+import yfinance as yf
+import datetime
+from waitress import serve
+
+app = Flask(__name__)
+
+
+@app.get("/api/stocks/<string:stock>")
+def stock_api(stock: str):
+    ticker = yf.Ticker(stock)
+    info = ticker.get_info()
+
+    if "regularMarketPrice" not in info:
+        return jsonify({"error": "Invalid ticker!"}), 400
+
+    previous_close = info["previousClose"]
+    current_price = info["regularMarketPrice"]
+    percent_change = round(((current_price - previous_close) / previous_close) * 100, 2)
+
+    historical_percents = calulcate_stock_changes(ticker)
+
+    return jsonify(
+        {
+            "stock": stock,
+            "price": info["regularMarketPrice"],
+            "percent": percent_change,
+            "percent_1mo": historical_percents["1mo"],
+            "percent_6mo": historical_percents["6mo"],
+            "percent_1y": historical_percents["1y"],
+        }
+    )
+
+
+def calulcate_stock_changes(ticker: yf.Ticker):
+    data = {}
+
+    for date in ["1mo", "6mo", "1y"]:
+        hist = ticker.history(date)
+        start_price = hist["Close"].iloc[0]
+        end_price = hist["Close"].iloc[-1]
+        pct_change = ((end_price - start_price) / start_price) * 100
+        data[date] = round(pct_change, 2)
+
+    return data
+
+
+if __name__ == "__main__":
+    print("Starting server...\t[http://127.0.0.1:9820]")
+    serve(app, host="127.0.0.1", port="9820")
